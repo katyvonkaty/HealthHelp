4f8bf9aef7b4d372fe63b00a9a1f01ff0a267fc2
katyvonkaty <kj0203@gmail.com>
2021-01-29 18:43:42 -0500

    updated

---
+++ src/containers/People/Popup.js
@@ -0,0 +1,26 @@
+import ReactMapGL, {Marker, Popup } from 'react-map-gl';
+
+const CustomPopup = ({index, marker, closePopup}) => {
+  return (
+    <Popup
+      latitude={marker.latitude}
+      longitude={marker.longitude}
+      onClose={closePopup}
+      closeButton={true}
+      closeOnClick={false}
+      offsetTop={-30}
+     >
+      <p>{marker.name}</p>
+    </Popup>
+  )};
+
+  const CustomMarker = ({index, marker, openPopup}) => {
+  return (
+    <Marker
+      longitude={marker.longitude}
+      latitude={marker.latitude}>
+      <div className="marker" onClick={() => openPopup(index)}>
+        <span><b>{index + 1}</b></span>
+      </div>
+    </Marker>
+)};
