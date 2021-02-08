97f7da49ce9258514c5e422dcf19d4beb5f81937
katyvonkaty <kj0203@gmail.com>
2021-02-08 11:50:12 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./containers/People/Home";
+
+
 
 import Profile from "./containers/People/Profile";
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from "axios";
-
-axios.defaults.baseURL = "https://api.imgflip.com/get_memes";
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
-axios.interceptors.request.use( request => {
-  console.log(request);
-  return request
-}, error => {
-  console.log(error)
-  return Promise.reject(error);
-})
 
 ReactDOM.render(
   <React.StrictMode>
